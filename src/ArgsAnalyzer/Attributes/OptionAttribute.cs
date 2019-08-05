cacd794b5ee1c76033fd4c454066c1cf41c1a816
uttne <uttne.mkr@gmail.com>
2019-08-06 02:02:57 +0900

    Update OptionAttribute.

--- src/ArgsAnalyzer/Attributes/OptionAttribute.cs
+++ src/ArgsAnalyzer/Attributes/OptionAttribute.cs
@@ -1,7 +1,29 @@
+using System;
+
 namespace ArgsAnalyzer.Attributes
 {
-    public class OptionAttribute
-    {
 
+    [AttributeUsage(AttributeTargets.Property)]
+    public class OptionAttribute : Attribute
+    {
+        /// <summary>
+        /// Option property attribute
+        /// </summary>
+        /// <param name="shortName"></param>
+        /// <param name="longName"></param>
+        /// <param name="description"></param>
+        /// <param name="isDefault"></param>
+        public OptionAttribute(char shortName = (char)0, string longName = null, string description = null, bool isDefault = false)
+        {
+            ShortName = shortName;
+            LongName = longName;
+            Description = description;
+            IsDefault = isDefault;
+        }
+
+        public string Description { get; }
+        public bool IsDefault { get; }
+        public char ShortName { get; }
+        public string LongName { get; }
     }
 }
