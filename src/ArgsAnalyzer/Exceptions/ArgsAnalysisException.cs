14740976dda4324a4db051be215b49291fa5e2aa
uttne <uttne.mkr@gmail.com>
2019-08-06 02:03:20 +0900

    Update ArgsAnalysisException.

--- src/ArgsAnalyzer/Exceptions/ArgsAnalysisException.cs
+++ src/ArgsAnalyzer/Exceptions/ArgsAnalysisException.cs
@@ -1,7 +1,23 @@
+using System;
+using System.Reflection;
+
 namespace ArgsAnalyzer.Exceptions
 {
-    public class ArgsAnalysisException
+    public class ArgsAnalysisException : Exception
     {
+        public ArgsAnalysisException(PropertyInfo analysisErrorPropertyInfo, string message, Exception innerException) : base(message, innerException)
+        {
+            AnalysisErrorPropertyInfo = analysisErrorPropertyInfo;
+        }
+        public ArgsAnalysisException(PropertyInfo analysisErrorPropertyInfo, string message) : base(message)
+        {
+            AnalysisErrorPropertyInfo = analysisErrorPropertyInfo;
+        }
+        public ArgsAnalysisException(PropertyInfo analysisErrorPropertyInfo)
+        {
+            AnalysisErrorPropertyInfo = analysisErrorPropertyInfo;
+        }
 
+        public PropertyInfo AnalysisErrorPropertyInfo { get; }
     }
 }
