2190911596d2fc745d3b862a5d14a027f3de5a65
uttne <uttne.mkr@gmail.com>
2019-08-25 00:47:12 +0900

    Add a token class of args.

---
+++ src/ArgsAnalyzer/Token.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArgsAnalyzer
+{
+
+
+    #region Token
+
+    public abstract class TokenBase
+    {
+
+    }
+
+    public sealed class OptionToken : TokenBase
+    {
+
+    }
+
+    public sealed class CommandToken : TokenBase
+    {
+
+    }
+
+    public sealed class ValueToken : TokenBase
+    {
+
+    }
+
+
+    #endregion
+
+}
