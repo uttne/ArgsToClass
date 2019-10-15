30a34616a8210d746d5396f34242882ad18e39b9
uttne <uttne.mkr@gmail.com>
2019-10-15 23:42:05 +0900

    Add sample code

--- src/Sample/Program.cs
+++ src/Sample/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using ArgsToClass;
+using ArgsToClass.Exceptions;
 
 namespace Sample
 {
+    public class Option
+    {
+        public bool Help { get; set; }
+        public string Name { get; set; }
+        public int Repeat { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // args == new string[]{ "--name", "test name", "--repeat", "1"};
+
+            var parser = new ArgsParser<Option>();
+            
+            IArgsData<Option> data;
+            try
+            {
+                // ここでコマンドライン引数を解析してクラスにマッピングする
+                data = parser.Parse(args);
+            }
+            catch (ArgsAnalysisException ex)
+            {
+                Console.WriteLine(ex);
+                return;
+            }
+
+            Console.WriteLine($"Help   == {data.Option.Help}");   // Help   == false
+            Console.WriteLine($"Name   == {data.Option.Name}");   // Name   == "test name"
+            Console.WriteLine($"Repeat == {data.Option.Repeat}"); // Repeat == 1
+
+            Console.ReadKey();
         }
     }
 }
